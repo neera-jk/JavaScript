c946d105f2ce2b501592fd87d35a56347195a657
Neeraj Kumar <neerajk811@gmail.com>
2024-05-05 11:29:20 +0530

    The codes I worked on JavaScript based on basics, loops, conditional statements and variables

---
+++ jsloops.js
@@ -0,0 +1,31 @@
+for (var i = 1; i <= 5; i++){
+    console.log(i)
+}
+console.log("Counting completed!")
+
+
+for(var i = 5; i>=1; i--){
+    console.log(i)
+}
+console.log("Countdown finished!")
+
+
+var counter = 0
+while(counter <= 5){
+    console.log(counter)
+}
+counter.log('Counting completed!')
+
+
+var counter = 5
+while(counter >= 0){
+    console.log(counter)
+}
+console.log("Countdown finished!")
+
+
+var counter = 2018
+while(counter <= 2022){
+    console.log(counter)
+    counter = counter + 1
+}
